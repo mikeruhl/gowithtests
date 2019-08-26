92fb0212903f25d63cc0333e96dcdfeae2b04ba4
Mike Ruhl <mike.ruhl@tobiidynavox.com>
2019-08-26 06:01:05 -0400

    finished pointers and errors

--- 06-pointers/main.go
+++ 06-pointers/main.go
@@ -1,18 +1,40 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 //Wallet is a wallet
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
+type Bitcoin int
+
 //Deposit adds to the balance
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+//Withdraw subtracts from the balance
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
 //Balance gets balance of wallet
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 func main() {}
